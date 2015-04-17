bdc9d520488e31788cec504b1d8ad60abb8b26d1
ta1kt0me <p.wadachi@gmail.com>
2015-04-18 00:03:56 +0900

    Add eval_let & let_to_parameters_args_body & let?

--- cap2/env.rb
+++ cap2/env.rb
@@ -9,3 +9,17 @@ def extend_env parameters, args, env
   alist = parameters.zip args
   [alist.to_h] + env
 end
+
+def eval_let exp, env
+  parameters, args, body = let_to_parameters_args_body exp
+  new_exp = [[:lambda, paramters, body]] + args
+  _eval(new_exp, env)
+end
+
+def let_to_parameters_args_body exp
+  [exp[1].map{|e| e[0]}, exp[1].map{|e| e[1]}, exp[2]]
+end
+
+def let? exp
+  exp[0] == :let
+end
